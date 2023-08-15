12e3893f8b09a7c11c78c0ad913cc96beee3bfbc
Marcelo Heredia <heredia_m@outlook.com>
2023-08-15 15:01:42 -0300

    added some extra configs and example component

---
+++ esquizito/src/components/SomeTest/SomeTest.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SomeTest() {
+  return (
+    <div>
+      <header>
+        <p>Edit and save to reload.</p>
+      </header>
+    </div>
+  );
+}
+
+export default SomeTest;
---
+++ esquizito/src/components/SomeTest/__tests__/SomeTest.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import { SomeTest } from 'components/SomeTest';
+
+test('renders learn react link', () => {
+  render(<SomeTest />);
+  const linkElement = screen.getByText(/Edit and save to reload./i);
+  expect(linkElement).toBeInTheDocument();
+});
