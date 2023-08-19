cd79127d2811edffe165ef5f85fa78b06af1ba6d
Eduardo Andrade <eduardo.a@edu.pucrs.br>
2023-08-19 02:44:58 -0300

    login page

--- esquizito/src/App.jsx
+++ esquizito/src/App.jsx
@@ -1,24 +1,11 @@
-import './App.scss';
-import { SomeTest } from 'components/SomeTest';
-import Component from 'components/Component';
-import logo from './logo.svg';
+import React from 'react';
+import LoginCard from 'components/LoginCard/LoginCard';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <Component />
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>God is less</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-      <SomeTest />
+    <div className={styles.app}>
+      <LoginCard />
     </div>
   );
 }
--- esquizito/src/index.jsx
+++ esquizito/src/index.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { CssVarsProvider, extendTheme } from '@mui/joy';
 import App from './App';
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: { 500: '#00394e' },
+      },
+    },
+  },
+  components: {
+    JoyButton: {
+      styleOverrides: {
+        root: { borderRadius: '50px' },
+      },
+    },
+    JoyCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: '38px',
+        },
+      },
+    },
+    JoyFormLabel: {
+      styleOverrides: {
+        root: {
+          fontSize: '1rem',
+        },
+      },
+    },
+    JoyInput: {
+      styleOverrides: {
+        root: {
+          borderRadius: '50px',
+        },
+      },
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CssVarsProvider theme={theme}>
+      <App />
+    </CssVarsProvider>
   </React.StrictMode>,
 );
