4ec04c77c96b820762ba4f6d6dd468a6b76308b9
Eduardo C. Andrade <eduardo.a@edu.pucrs.br>
2023-08-20 14:35:38 -0300

    Update esquizito/src/index.jsx
    Co-authored-by: Marcelo Heredia <42178819+MarceloHeredia@users.noreply.github.com>

--- esquizito/src/index.jsx
+++ esquizito/src/index.jsx
@@ -8,7 +8,7 @@ import theme from './Theme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CssVarsProvider theme={Theme}>
+    <CssVarsProvider theme={theme}>
       <App />
     </CssVarsProvider>
   </React.StrictMode>,
